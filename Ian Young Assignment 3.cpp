038d19fb5e23587bf3a87a690c393fb32de99bcd
Ian-Young <ianyoung0107@gmail.com>
2022-10-26 18:36:41 -0600

    Skeleton: Just empty classes

---
+++ Ian Young Assignment 3.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+
+
+class Customer {
+
+};
+
+
+class Cart {
+
+};
+
+
+class Order {
+
+};
+
+
+class Inventory {
+
+};
+
+
+class Product {
+// Acts as an Interface
+
+};
+
+
+class Apple {
+
+};
+
+
+class Banana {
+
+};
+
+
+class Orange {
+
+};
+
+
+int main() {
+
+return 0;
+}
+
